9468826183ab99b0d37b4de47decf56d1d7749fc
holi <asadasd@ajsdjsad.xom>
2019-04-17 02:19:28 -0500

    Pintamos data completa y filtrada en pantalla

--- src/data.js
+++ src/data.js
@@ -1,6 +1,11 @@
 // Funciones 
-const searchMovie = (arrData, range) => {
+const filterMovie1999 = (arrData, range) => {
     return arrData.filter(object => object.Year <= range);
     };
-    window.searchMovie = searchMovie;
+    window.filterMovie1999 = filterMovie1999;
+
+const filterMovie2000 = (arrData, range) => {
+    return arrData.filter(object => object.Year >= range);
+    };
+    window.filterMovie2000 = filterMovie2000;
 
