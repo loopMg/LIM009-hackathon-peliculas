c9735e4f80fb17e7e3f60cfc1a3b9e411a92368b
holi <asadasd@ajsdjsad.xom>
2019-04-17 11:04:37 -0500

    todo ok

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ btnStart.addEventListener('click', () => {
     navOpt.style.display = 'none';
 });
 
-btnMovies.addEventListener('click', () =>{
+btnMovies.addEventListener('click', () => {
     startPage.style.display = 'none';
     moviesPage.style.display = 'block';
     navOpt.style.display = 'block';
@@ -31,59 +31,59 @@ const urlStarWars = 'https://www.omdbapi.com/?s=star-wars&apikey=ec75ba49';
 
 // Imprime toda la data primera historia de usurio 
 fetch(urlStarWars)
-.then(response => response.json())
-.then(data => {
-    const dataMovie = data.Search;
-    let string = '';
-    for(let i = 0; i < dataMovie.length; i++) {
-        string +=  `<div class="movie-box" >
+    .then(response => response.json())
+    .then(data => {
+        const dataMovie = data.Search;
+        let string = '';
+        for (let i = 0; i < dataMovie.length; i++) {
+            string += `<div class="movie-box" >
         <img src="${dataMovie[i].Poster}" alt="poster" id="${dataMovie[i].imdbID}" class="poster-movie"/>
         <h1>${dataMovie[i].Title}</h1>
         <h2>(${dataMovie[i].Year})</h2> 
       </div>
-    `; 
-    }
-    console.log(string);
-    moviesPage.innerHTML = string;
-})
-.catch(err => console.log(err));
+    `;
+        }
+        console.log(string);
+        moviesPage.innerHTML = string;
+    })
+    .catch(err => console.log(err));
 
 // Imprimir lo filtrado
 fetch(urlStarWars)
-.then(response => response.json())
-.then(data => {
-    const dataMovie = data.Search;
-    let newArrfilter = '';
-    optFilter.addEventListener('change', (event) => {
-        if( event.target.value < 2000) {
-            newArrfilter = window.filterMovie1999(dataMovie, event.target.value);
-            let string = '';
-            for(let i = 0; i < newArrfilter.length; i++) {
-            string +=  `<div class="movie-box" >
+    .then(response => response.json())
+    .then(data => {
+        const dataMovie = data.Search;
+        let newArrfilter = '';
+        optFilter.addEventListener('change', (event) => {
+            if (event.target.value < 2000) {
+                newArrfilter = window.filterMovie1999(dataMovie, event.target.value);
+                let string = '';
+                for (let i = 0; i < newArrfilter.length; i++) {
+                    string += `<div class="movie-box" >
             <img src="${newArrfilter[i].Poster}" alt="poster" class="poster-movie"/>
             <h1>${newArrfilter[i].Title}</h1>
             <h2>(${newArrfilter[i].Year})</h2>
         </div>
         `;
-    } 
-    console.log(string)
-    moviesPage.innerHTML = string;
-    }else {
-        newArrfilter = window.filterMovie2000(dataMovie, event.target.value);
-        let string = '';
-        for(let i = 0; i < newArrfilter.length; i++) {
-            string +=  `<div class="movie-box">
+                }
+                console.log(string)
+                moviesPage.innerHTML = string;
+            } else {
+                newArrfilter = window.filterMovie2000(dataMovie, event.target.value);
+                let string = '';
+                for (let i = 0; i < newArrfilter.length; i++) {
+                    string += `<div class="movie-box">
             <img src="${newArrfilter[i].Poster}" alt="poster" class="poster-movie"/>
             <h1>${newArrfilter[i].Title}</h1>
             <h2>(${newArrfilter[i].Year})</h2> 
         </div>    
         `;
-    }
-    console.log(string)
-    moviesPage.innerHTML = string;
-}
-    }); 
-    
-})
-.catch(err => console.log(err));
+                }
+                console.log(string)
+                moviesPage.innerHTML = string;
+            }
+        });
+
+    })
+    .catch(err => console.log(err));
 
